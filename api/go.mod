e24af9ccf5b618db02d88a6b3f117deaad98bac0
Arvindeva Wibisono <arvindeva@gmail.com>
2024-02-12 15:30:15 +0100

    add dotenv, ready to test deployment

--- api/go.mod
+++ api/go.mod
@@ -3,3 +3,5 @@ module github.com/arvindeva/touhouapi/api
 go 1.21.6
 
 require github.com/gorilla/mux v1.8.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- api/main.go
+++ api/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
 func main() {
+	port, exists := os.LookupEnv("PORT")
+	if exists {
+		fmt.Println(port)
+	} else {
+		return
+	}
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Hello world!")
 	})
 
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":"+port, r)
 }
